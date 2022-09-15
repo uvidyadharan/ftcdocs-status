67cbdabeb6e5492906a1fb45ec15ee4d5ecb1050
Uday Vidyadharan <55924756+uvidyadharan@users.noreply.github.com>
2022-09-15 19:57:59 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import os
 from pushbullet import PushBullet
+import subprocess
+import pickle
 
 access_token = os.getenv('PUSHBULLET_ACCESS_TOKEN')
 pb = PushBullet(access_token)
+old_errors = []
 
 
 while True:
@@ -11,10 +14,22 @@ while True:
     status = os.subprocess.call('make', '-C docs/ linkcheck SPHINXOPTS="-W --keep-going -n"')
 
     if status != 0:
+        try:        
+            with open('errors.pickle', 'rb') as f:
+                old_errors = pickle.load(f)
+        except:
+            print("Could not load old errors")
+            
         pb.push_note('FTC Docs Link Check failed', 'The FTC Docs Link Check failed. Please check the build logs.')
 
         with open("ftcdocs/docs/build/linkcheck/output.txt", "rb") as log:
-            log_data = pb.upload_file(log, "log.txt")
+            errors = [(line.strip()).split() for line in log]
 
-        pb.push_file(**log_data)
+        if old_errors != errors:
+            for error in errors:
+                if error not in old_errors:
+                    pb.push_link("New Error", error)
+                    
+            with open('errors.pickle', 'wb') as f:
+                pickle.dump(error, f)
 
